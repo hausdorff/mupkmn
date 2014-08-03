4663d519a64cd15824027dac9e7fa78291abdfa1
Alex Clemmer <clemmer.alexander@gmail.com>
2014-08-03 16:48:34 -0700

    Add filter functions for Pointset

--- code/pointset.ml
+++ code/pointset.ml
@@ -1,8 +1,11 @@
 open Core.Std
 
 type t = Point.t Set.Poly.t
+let empty = Set.Poly.empty
 let singleton pt = Set.Poly.singleton pt
 let add t pt = Set.Poly.add t pt
 let of_list list = Set.Poly.of_list list
 let iter t ~f = Set.Poly.iter t ~f:f
 let fold t ~init ~f = Set.Poly.fold t ~init:init ~f:f
+let filter t ~f = Set.Poly.filter t ~f:f
+let to_list t = Set.Poly.fold t ~init:[] ~f:(fun accum pt -> pt::accum)
--- code/pointset.mli
+++ code/pointset.mli
@@ -1,8 +1,11 @@
 (* A set of points. *)
 
 type t
+val empty     : t
 val singleton : Point.t -> t
-val add : t -> Point.t -> t
-val of_list : Point.t list -> t
-val iter : t -> f:(Point.t -> unit) -> unit
-val fold : t -> init:'accum -> f:('accum -> Point.t -> 'accum) -> 'accum
+val add       : t -> Point.t -> t
+val of_list   : Point.t list -> t
+val iter      : t -> f:(Point.t -> unit) -> unit
+val fold      : t -> init:'accum -> f:('accum -> Point.t -> 'accum) -> 'accum
+val filter    : t -> f:(Point.t -> bool) -> t
+val to_list   : t -> Point.t list
