7a1632b6c333b9356d175aa9e8c5dddee14d0fd6
Alex Clemmer <clemmer.alexander@gmail.com>
2014-08-04 06:54:55 -0400

    Cosmetic changes to Point_assoc.mli

--- code/point_assoc.mli
+++ code/point_assoc.mli
@@ -7,6 +7,7 @@ val add_multi      : t -> Point.t -> Point.t -> t
 val of_list_exn    : (Point.t * Pointset.t) list -> t
 val to_list        : t -> (Point.t * Point.t) list
 val iter           : t -> f:(Point.t -> Point.t -> unit) -> unit
-val filter_edges   : t -> f:(Point.t -> Point.t -> bool) -> (Point.t * Point.t) list
+val filter_edges
+    : t -> f:(Point.t -> Point.t -> bool) -> (Point.t * Point.t) list
 val filter_adj_pts
     : t -> Point.t -> f:(Point.t -> Point.t -> bool) -> Pointset.t
