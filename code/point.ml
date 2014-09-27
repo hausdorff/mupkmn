c51a313a488a772ce16183ed47b42dde8be542e4
Alex Clemmer <clemmer.alexander@gmail.com>
2014-09-27 13:05:22 -0700

    Update point API

--- code/point.ml
+++ code/point.ml
@@ -2,6 +2,7 @@ open Core.Std
 
 type t = (int*int)
 let create x y = (x,y)
+let of_tup (x,y) = (x,y)
 let x t = fst t
 let y t = snd t
 let to_string (x,y) =
--- code/point.mli
+++ code/point.mli
@@ -2,6 +2,7 @@
 
 type t
 val create : int -> int -> t
+val of_tup : (int * int) -> t
 val x : t -> int
 val y : t -> int
 val to_string : t -> string
