3eb06dbf9003789acf54831834473d10e88e3c7c
Alex Clemmer <clemmer.alexander@gmail.com>
2014-09-27 13:03:44 -0700

    Update edge public API

--- code/edge.ml
+++ code/edge.ml
@@ -1,5 +1,13 @@
+open Core.Std
+
 type t = { pt1: Point.t; pt2: Point.t; label: Regex.t }
 let create pt1 pt2 label = { pt1 = pt1; pt2 = pt2; label = label }
 let pt1 t = t.pt1
 let pt2 t = t.pt2
 let label t = t.label
+let to_string t =
+  String.concat [ (Point.to_string t.pt1);
+                  " -> ";
+                  (Point.to_string t.pt2);
+                  " : ";
+                  (Regex.to_string t.label); ]
--- code/edge.mli
+++ code/edge.mli
@@ -5,3 +5,4 @@ val create : Point.t -> Point.t -> Regex.t -> t
 val pt1 : t -> Point.t
 val pt2 : t -> Point.t
 val label : t -> Regex.t
+val to_string : t -> string
