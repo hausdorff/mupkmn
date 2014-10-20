dd373aa4f43d396157b5330837f3682ac0fbe5b0
Alex Clemmer <clemmer.alexander@gmail.com>
2014-10-19 19:37:27 -0700

    Add convenience method to Edgeset

--- code/edgeset.ml
+++ code/edgeset.ml
@@ -6,8 +6,14 @@ let empty = Map.Poly.empty
 
 let add t pt1 pt2 regex = Map.Poly.add t ~key:(pt1,pt2) ~data:regex
 
+let singleton pt1 pt2 regex = add empty pt1 pt2 regex
+
 let find_exn t pt1 pt2 =
   Edge.create
     pt1
     pt2
     (Map.Poly.find_exn t (pt1,pt2))
+
+let find t pt1 pt2 = match (Map.Poly.find t (pt1,pt2)) with
+    None -> None
+  | Some e -> Some (Edge.create pt1 pt2 e)
--- code/edgeset.mli
+++ code/edgeset.mli
@@ -1,4 +1,6 @@
 type t
-val empty    : t
-val add      : t -> Point.t -> Point.t -> Regex.t -> t
-val find_exn : t -> Point.t -> Point.t -> Edge.t
+val empty     : t
+val add       : t -> Point.t -> Point.t -> Regex.t -> t
+val singleton : Point.t -> Point.t -> Regex.t -> t
+val find_exn  : t -> Point.t -> Point.t -> Edge.t
+val find      : t -> Point.t -> Point.t -> Edge.t option
